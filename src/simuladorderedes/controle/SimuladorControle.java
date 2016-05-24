6a21e5de4291147d06cbdd8ff7b8cf06bf8dd2a6
Filipe <libertadores81@gmail.com>
2016-05-24 15:40:19 -0300

    Commit 15:40 24/05 JinternalFrame com botao e imagem

--- src/simuladorderedes/controle/SimuladorControle.java
+++ src/simuladorderedes/controle/SimuladorControle.java
@@ -1,14 +1,7 @@
 package simuladorderedes.controle;
  
 
-import java.awt.HeadlessException;
-import java.awt.Image;
-import java.awt.Toolkit;
-import javax.swing.JFrame;
-import javax.swing.RepaintManager;
 import simuladorderedes.visao.TelaPrincipal;
-
-import simuladorderedes.visao.*;
 
 /**
  *
