669c298577da090a3fafd1d18035e688590ee899
labccet <labccet@10.0.210.157>
2016-05-24 21:10:04 -0300

    teste merge

---
+++ src/simuladorderedes/modelo/InterfaceFisica.java
@@ -0,0 +1,15 @@
+package simuladorderedes.modelo;
+/**
+ *
+ * @author Filipe
+ */
+enum Interaface{
+    Ethernet;
+}
+class InterfaceFisica {
+
+    
+    public InterfaceFisica() {
+    }
+    
+}
--- src/simuladorderedes/modelo/equipamento/Equipamento.java
+++ src/simuladorderedes/modelo/equipamento/Equipamento.java
@@ -1,13 +1,34 @@
 package simuladorderedes.modelo.equipamento;
 
-/**
- *
- * @author Filipe
- */
-public class Equipamento{
-   
-    private int mac;
+import simuladorderedes.modelo.EnderecoMac;
+
+public abstract class Equipamento {
+
+    private EnumTipoEquipamento tipo;
+    private String nome;
+    private final EnderecoMac mac;
     private int portaEthernet;
     private boolean ligado;
-    
+
+    public Equipamento(EnumTipoEquipamento tipo, String nome) {
+        this.mac = new EnderecoMac();
+        this.tipo = tipo;
+        this.nome = nome;
+        this.ligado = true;
+    }
+    public abstract void broadcast();
+
+    public EnderecoMac getMac() {
+        return mac;
+    }
+    public void ligaDesliga(){
+        ligado = !ligado;
+        System.out.println(ligado);
+    }
+    public String getTipo(){
+        return tipo.name();
+    }
+    public String getNome(){
+        return this.nome;
+    }
 }
